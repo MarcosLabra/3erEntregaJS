f15f840eff3fcdbb3a9f1710021f3f7409305b22
MarcosLabra <mlabra.dev@gmail.com>
2022-02-05 12:14:41 -0300

    agrego render carrito con funcionalidad de agregar y quitar productos, tmb cuenta cantidades

---
+++ script/carrito.js
@@ -0,0 +1,50 @@
+function agregarAlCarrito(indiceDelArrayProducto){
+    const indiceEncontradoCarrito = carrito.findIndex((elemento) => {
+      return elemento.id === tienda[indiceDelArrayProducto].id;
+    });
+    if (indiceEncontradoCarrito === -1) {
+      const productoAgregar = tienda[indiceDelArrayProducto];
+      productoAgregar.cantidad = 1;
+      carrito.push(productoAgregar);
+      actualizarStorage(carrito);
+      renderCarrito();
+    } else {
+      carrito[indiceEncontradoCarrito].cantidad += 1;
+      actualizarStorage(carrito);
+      renderCarrito();
+    }
+  };
+  
+  function actualizarStorage(carrito){
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  };
+
+function renderCarrito(){
+    $("#contenedorCarrito").empty();
+    carrito.forEach((carrito, indice) => {
+    
+        $("#contenedorCarrito").append(`<div class="cartItem">
+                                            <img src="${carrito.imagen}" alt="...">
+                                            <div class="itemDescription">
+                                                <h3>${carrito.nombre}</h3>
+                                                <div>
+                                                    <p>x${carrito.cantidad}</p>
+                                                    <p>$${carrito.precio}</p>
+                                                    <h4>$${carrito.precio * carrito.cantidad}</h4>
+                                                </div>
+                                                <button class="btn" onClick="removeProduct(${indice})"><i class="fas fa-trash-alt"></i></button>
+                                            </div>
+                                        </div>`);
+        });
+        const precioTotal = carrito.reduce((precioTotal, carrito) => precioTotal + carrito.precio*carrito.cantidad, 0);
+        $("#valorTotal").html(`Total: $ ${precioTotal}`)
+    
+    }
+  
+    function removeProduct(indice){
+      carrito.splice(indice, 1);
+      actualizarStorage(carrito);
+      $("#contenedorCarrito").empty();
+      renderCarrito();
+       };
+  
--- script/main.js
+++ script/main.js
@@ -1,23 +1,5 @@
 
 // ------------------Funciones--------------------->
-function agregarAlCarrito(indiceDelArrayProducto){
-  const indiceEncontradoCarrito = carrito.findIndex((elemento) => {
-    return elemento.id === tienda[indiceDelArrayProducto].id;
-  });
-  if (indiceEncontradoCarrito === -1) {
-    const productoAgregar = tienda[indiceDelArrayProducto];
-    productoAgregar.cantidad = 1;
-    carrito.push(productoAgregar);
-    actualizarStorage(carrito);
-  } else {
-    carrito[indiceEncontradoCarrito].cantidad += 1;
-    actualizarStorage(carrito);
-  }
-};
-
-function actualizarStorage(productos){
-  localStorage.setItem("carrito", JSON.stringify(productos));
-};
 
 function filtrarCategoria(id){
   $(`#${id}`).click (() => {
@@ -50,31 +32,42 @@ $(".btnCarrito").click(() => {
   $("#carrito").toggle();
 })
 // ----------------------MAIN----------------------------->
-let tienda = [];
 let carrito = [];
 
 if (localStorage.getItem("carrito")) {
   carrito = JSON.parse(localStorage.getItem("carrito"));
+  renderCarrito();
 }
+
+let tienda = [];
 
 $.ajax({
   method:"GET",
   url: `../JSON/general.json`
   ,success: function (respuesta) {
-    respuesta.forEach((producto, indice) => {
-      $("#contenedorProductos").append(`<div class="card">
-      <img src="${producto.imagen}" alt="producto">
-      <h3>${producto.nombre}</h3>
-      <h6>${producto.material}</h6>
-      <p>$${producto.precio}</p>
-      <button class= "btn" onClick="agregarAlCarrito(${indice})">Comprar</button>
-    </div>`)
+    $("#contenedorProductos").empty();
+    tienda = respuesta;
+   tienda.forEach((producto, indice) => {
+     $("#contenedorProductos").append(`<div class="card">
+     <img src="${producto.imagen}" alt="producto">
+     <h3>${producto.nombre}</h3>
+     <h6>${producto.material}</h6>
+     <p>$${producto.precio}</p>
+     <button class= "btn" onClick="agregarAlCarrito(${indice})">Comprar</button>
+   </div>`)
     })
   }
 })
 
 filtrarCategoria("nostril");
 filtrarCategoria("argollitas");
+
+
+$("#vaciarCarrito").click(function vaciarCarrito(){
+                              localStorage.removeItem("carrito");
+                              carrito = [];
+                            $("#valorTotal").html(`<h3 id="valorTotal">Total: $0</h3>`)
+                            $("#contenedorCarrito").empty()});
 
 
 
